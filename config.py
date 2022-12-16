14ba7b9cc33280daff942766d34221ef11278d7b
choisaelim <choisaelim@gmail.com>
2022-12-16 18:32:06 +0900

    설정파일 수정

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BLOG_URL = "https://blog.naver.com/mae0436"
-chrome_driver = "C:\\Users\\saelim\\Downloads\\ska1018-1-1-article1-488\\chromedriver\\chromedriver.exe"
-img_root_path = 'C:\\Users\\saelim\\Downloads\\ska1018-1-1-article1-488\\ska1018-1-1\\'
+BLOG_URL = ""
+chrome_driver = ""
+img_root_path = ""
